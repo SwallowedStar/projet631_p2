39f6dc14045c6c6b63718348ce56d5fcd8051cf5
etienne.malacarne@gmail.com <etienne.malacarne@gmail.com>
2022-05-03 16:36:33 +0200

    remove some code

--- Main.java
+++ Main.java
@@ -16,33 +16,14 @@ public class Main {
         System.out.println(l_res);
         /*List<String> l_res2=create_list_freq(content);
         System.out.println(l_res2);*/
+        create_list_trie(l_res);
 
 
 
     }
 
-
     public static String get_content_file(String filename) throws Exception{
-        /*File doc =new File(filename);
-        Scanner obj = new Scanner(doc);
-        String str="";
-        while (obj.hasNextLine()){
-            if(obj.hasNextLine()){
-                str+=obj.nextLine();
-            }
-
-        }*/
         String content = new String(Files.readAllBytes(Paths.get(filename)));
-        /*FileReader fr =
-                new FileReader(filename);
-
-        int i;
-        String str="";
-        while ((i=fr.read()) != -1){
-            str+=((char) i);
-        }*/
-
-
         return content;
     }
 
@@ -56,23 +37,4 @@ public class Main {
 
         return frequency;
     }
-
-    /*public static List<String> create_list_freq(String content){
-        List<String> l_freq= new ArrayList<>();
-        List<String> l_cara=new ArrayList<>();
-        for (int i=0;i<content.length();i++){
-            char une_l=content.charAt(i);
-            if(!l_cara.contains(String.valueOf(une_l))){
-                //System.out.println(String.valueOf(une_l));
-                long freq=content.chars().filter(ch->ch==une_l).count();
-                String freq2=String.valueOf(freq);
-                l_cara.add(String.valueOf(une_l));
-                l_freq.add(freq2);
-            }
-
-        }
-
-        return l_freq;
-    }*/
-
 }
